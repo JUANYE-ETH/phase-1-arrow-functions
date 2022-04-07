07d7fb283cdac4382c8283e9cb1045dbbc78c6f7
Juan Velasco <juanayevela@yahoo.com>
2022-04-07 16:54:12 -0400

    Done.

---
+++ index.js
@@ -0,0 +1,11 @@
+const divide = param1 => 2000 / 100;
+console.log(divide());
+
+const square = x => x * x;
+let x = 2;
+console.log(square(x));
+
+const add = (a, b) => a + b;
+let a = 3
+let b = 4
+console.log(add(a,b));
